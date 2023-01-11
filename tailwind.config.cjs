f4eff3e61c4bfb91151627949c8afc1639879c70
Scott Hoecker <hoeckers@gmail.com>
2023-01-11 17:50:11 -0600

    Add contact form

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,16 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
       colors: {
         mainBackground: '#282828',
         lightBackground: '#665c54',
         mainText: '#dfbf8e',
         niceGreen: '#a9b665',
         niceBlue: '#7daea3',
-      }
+      },
     },
-	},
-	plugins: [],
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant('child', '& > *')
+      addVariant('child-hover', '& > *:hover')
+    },
+  ],
 }

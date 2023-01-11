cdca4e2dc38eaaa7e74c816432aa999cf1ce29d4
Scott Hoecker <hoeckers@gmail.com>
2023-01-11 11:18:33 -0600

    About me, layout, colorscheme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+      colors: {
+        mainBackground: '#282828',
+        lightBackground: '#665c54',
+        mainText: '#dfbf8e',
+        niceGreen: '#a9b665',
+        niceBlue: '#7daea3',
+      }
+    },
 	},
 	plugins: [],
 }

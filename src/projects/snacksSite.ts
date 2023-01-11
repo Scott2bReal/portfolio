6dbdddd5ddd8d0163893d65caae93a42d4603b69
Scott Hoecker <hoeckers@gmail.com>
2023-01-11 16:31:49 -0600

    Improve accessibility and SEO

--- src/projects/snacksSite.ts
+++ src/projects/snacksSite.ts
@@ -1,7 +1,7 @@
 import type { Project } from '../types'
 
-const description = `A website for my band, Nasty Snacks. Built using Astro,
-and Sanity, integrated with a Discord bot so we can update the site via slash
+const description = `A website for my band, Nasty Snacks. Built using Astro and
+Sanity, integrated with a Discord bot so we can update the site via slash
 commands`
 
 export const snacksSite: Project = {

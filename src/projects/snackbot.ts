bb50d2cfa3c00dcd1959e3b3837330484d231fda
Scott Hoecker <hoeckers@gmail.com>
2023-01-12 22:20:28 -0600

    Tweak project description

--- src/projects/snackbot.ts
+++ src/projects/snackbot.ts
@@ -1,7 +1,7 @@
-import type {Project} from '../types'
+import type { Project } from '../types'
 
-const description = `First a Slack bot, and now a Discord bot, to help automate
-workflows for my band, such as updating our website or managing scheduling`
+const description = `First a Slack bot, now a Discord bot. Helps automate
+workflows for my band, such as updating our website and managing scheduling`
 
 export const snackbot: Project = {
   projectName: 'SNACKBOT',

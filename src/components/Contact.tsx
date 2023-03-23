dcc257a2068e66ea8d90461b9cacd2540a14f7c5
Scott Hoecker <hoeckers@gmail.com>
2023-03-23 06:31:04 -0500

    Remove console message, update deps

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -12,9 +12,6 @@ export default function Contact() {
   const [message, setMessage] = createSignal('')
   const [botField, setBotField] = createSignal('')
   const [isSubmitted, setIsSubmitted] = createSignal(false)
-  console.log(
-    `Hi there, thanks for checking out my site! I'm always happy to collaborate and meet new people, so please hit me up using the contact form if you're so inclined`
-  )
 
   // Form handling
   const isFormComplete = (inputs: Accessor<string>[]) => {

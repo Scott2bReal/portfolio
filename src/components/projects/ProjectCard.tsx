ab7d4fcf582b6639ea500006a1f97f2f8bcf9b9c
Scott Hoecker <hoeckers@gmail.com>
2023-03-23 07:46:44 -0500

    Update link rels

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -1,59 +1,57 @@
-import { createSignal, Signal } from 'solid-js'
-import type { Project } from '../../types'
+import { createSignal, Signal } from "solid-js";
+import type { Project } from "../../types";
 
 interface Props {
-  project: Project
-  anyHovered: Signal<boolean>
+  project: Project;
+  anyHovered: Signal<boolean>;
 }
 
 export default function ProjectCard({ project, anyHovered }: Props) {
-  const { projectName, projectDescription, projectLink } = project
-  const [isHovered, setIsHovered] = createSignal(false)
-  const [isAnyHovered, setIsAnyHovered] = anyHovered
+  const { projectName, projectDescription, projectLink } = project;
+  const [isHovered, setIsHovered] = createSignal(false);
+  const [isAnyHovered, setIsAnyHovered] = anyHovered;
 
   return (
     <div
-      class='project-card'
+      class="project-card"
       onMouseEnter={() => {
-        setIsHovered(true)
-        setIsAnyHovered(true)
+        setIsHovered(true);
+        setIsAnyHovered(true);
       }}
       onMouseLeave={() => {
-        setIsHovered(false)
-        setIsAnyHovered(false)
+        setIsHovered(false);
+        setIsAnyHovered(false);
       }}
       id={projectName + projectLink}
     >
       <div
-        class={`transition duration-500 ease-in-out ${
-          isAnyHovered() && !isHovered() ? `opacity-50` : ``
-        }`}
+        class={`transition duration-500 ease-in-out ${isAnyHovered() && !isHovered() ? `opacity-50` : ``
+          }`}
       >
-        <div class=''>
-          <a href={projectLink} target='_blank'>
-            <h3 class='inline text-2xl font-bold'>{projectName}</h3>
+        <div class="">
+          <a href={projectLink} target="_blank" rel="noopener noreferrer">
+            <h3 class="inline text-2xl font-bold">{projectName}</h3>
             <svg
-              xmlns='http://www.w3.org/2000/svg'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke-width='1.5'
-              stroke='currentColor'
-              class={`${
-                isHovered() ? `animate-bounce` : ``
-              } inline h-6 w-6 -translate-y-1 text-niceBlue hover:opacity-50`}
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class={`${isHovered() ? `animate-bounce` : ``
+                } inline h-6 w-6 -translate-y-1 text-niceBlue hover:opacity-50`}
             >
               <path
-                stroke-linecap='round'
-                stroke-linejoin='round'
-                d='M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25'
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
               ></path>
             </svg>
           </a>
         </div>
-        <p class='mx-auto max-w-[80%] opacity-90 lg:max-w-full'>
+        <p class="mx-auto max-w-[80%] opacity-90 lg:max-w-full">
           {projectDescription}
         </p>
       </div>
     </div>
-  )
+  );
 }

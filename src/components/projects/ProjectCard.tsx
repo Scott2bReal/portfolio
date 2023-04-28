ad21e4bfdc99bfcce96a35307593c02bb6ba3c33
Scott Hoecker <hoeckers@gmail.com>
2023-04-27 19:01:36 -0500

    Remove unused code, change project cards

--- src/components/projects/ProjectCard.tsx
+++ src/components/projects/ProjectCard.tsx
@@ -12,7 +12,10 @@ export default function ProjectCard({ project, anyHovered }: Props) {
   const [isAnyHovered, setIsAnyHovered] = anyHovered;
 
   return (
-    <div
+    <a
+      href={projectLink}
+      target="_blank"
+      rel="noopener noreferrer"
       class="project-card"
       onMouseEnter={() => {
         setIsHovered(true);
@@ -28,30 +31,11 @@ export default function ProjectCard({ project, anyHovered }: Props) {
         class={`transition duration-500 ease-in-out ${isAnyHovered() && !isHovered() ? `opacity-50` : ``
           }`}
       >
-        <div class="">
-          <a href={projectLink} target="_blank" rel="noopener noreferrer">
-            <h3 class="inline text-2xl font-bold">{projectName}</h3>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class={`${isHovered() ? `animate-bounce` : ``
-                } inline h-6 w-6 -translate-y-1 text-niceBlue hover:opacity-50`}
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-              ></path>
-            </svg>
-          </a>
-        </div>
+        <h3 class={`inline text-2xl font-bold`}>{projectName}</h3>
         <p class="mx-auto max-w-[80%] opacity-90 lg:max-w-full">
           {projectDescription}
         </p>
       </div>
-    </div>
+    </a>
   );
 }

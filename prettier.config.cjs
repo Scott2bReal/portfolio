0ba7a244cb8f497be52df25170f2f0de4173cbc0
Scott Hoecker <hoeckers@gmail.com>
2024-04-26 18:25:56 -0500

    Add format scripts, update prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -9,9 +9,9 @@ module.exports = {
       },
     }
   ],
+  semi: false,
   plugins: [
     require.resolve("prettier-plugin-astro"),
     require.resolve("prettier-plugin-tailwindcss"),
   ],
-  pluginSearchDirs: false
 };

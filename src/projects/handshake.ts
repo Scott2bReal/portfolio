08de5955f426560646b5c6cd636cce9d336ae052
Scott Hoecker <hoeckers@gmail.com>
2024-04-26 21:39:45 -0500

    Copy updates

--- src/projects/handshake.ts
+++ src/projects/handshake.ts
@@ -4,5 +4,5 @@ export const handshake: Project = {
   projectLink: "https://joinhandshake.com",
   projectName: "Handshake",
   projectDescription:
-    "For the past year, I have been working on Handshake's marketing site as a full-stack engineer. I have been responsible for building out new features, implementing world-class design on high-profile pages, and assisting with their transition to a new tech stack",
+    "For the past year, I have been working on Handshake's marketing site as a full-stack engineer. I have been responsible for building out new features, implementing design on high-profile pages, and assisting with their transition to a new tech stack",
 }
--- src/projects/sva.ts
+++ src/projects/sva.ts
@@ -3,5 +3,5 @@ import type { Project } from "../types"
 export const sva: Project = {
   projectName: `School of Visual Arts`,
   projectLink: `https://branding.sva.edu`,
-  projectDescription: `I contributed to this award-nominated site in 2023. Working on this site was my first real exposure to Three.js and WebGL. I helped fix bugs with the cursor tracer, built out a dashboard so stakeholders could set up redirects, and helped the third-party scripts from their old site working on this new stack`,
+  projectDescription: `I contributed to this award-nominated site in 2023`,
 }

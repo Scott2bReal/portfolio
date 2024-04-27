8b070bc202b3f9870839a739d0eb4c962ecb0504
Scott Hoecker <hoeckers@gmail.com>
2024-04-26 22:20:53 -0500

    Copy updates and style tweaks

--- src/projects/sva.ts
+++ src/projects/sva.ts
@@ -3,5 +3,5 @@ import type { Project } from "../types"
 export const sva: Project = {
   projectName: `School of Visual Arts`,
   projectLink: `https://branding.sva.edu`,
-  projectDescription: `I contributed to this award-nominated site in 2023`,
+  projectDescription: `I contributed to this award-nominated site in 2023. Helping fix bugs with the cursor tracer and buliding out a dashboard so stakeholders could set up redirects were among the tasks I completed`,
 }

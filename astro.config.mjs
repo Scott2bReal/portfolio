0dbe7b17ad2341f33e2811538a358825d6ac1680
Scott Hoecker <hoeckers@gmail.com>
2024-09-25 12:06:47 -0500

    Update partytown config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,22 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
-import solidJs from '@astrojs/solid-js'
+import solidJs from "@astrojs/solid-js"
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), solidJs(), partytown()],
+  integrations: [
+    tailwind(),
+    solidJs(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 })
